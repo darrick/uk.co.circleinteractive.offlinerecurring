0d58e87c2b4e8fb968f700e129c8198032ac729f
Pradeep Nayak <Pradeep Nayak>
2018-06-18 15:23:40 +0100

    fixed civix file

--- offlinerecurring.civix.php
+++ offlinerecurring.civix.php
@@ -6,10 +6,10 @@
  * The ExtensionUtil class provides small stubs for accessing resources of this
  * extension.
  */
-class _ExtensionUtil {
+class CRM_Offlinerecurring_ExtensionUtil {
   const SHORT_NAME = "offlinerecurring";
   const LONG_NAME = "uk.co.circleinteractive.offlinerecurring";
-  const CLASS_PREFIX = "";
+  const CLASS_PREFIX = "CRM_Offlinerecurring";
 
   /**
    * Translate a string using the extension's domain.
@@ -77,7 +77,7 @@ class _ExtensionUtil {
 
 }
 
-use _ExtensionUtil as E;
+use CRM_Offlinerecurring_ExtensionUtil as E;
 
 /**
  * (Delegated) Implements hook_civicrm_config().
@@ -205,14 +205,14 @@ function _offlinerecurring_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = N
 }
 
 /**
- * @return _Upgrader
+ * @return CRM_Offlinerecurring_Upgrader
  */
 function _offlinerecurring_civix_upgrader() {
-  if (!file_exists(__DIR__ . '//Upgrader.php')) {
+  if (!file_exists(__DIR__ . '/CRM/Offlinerecurring/Upgrader.php')) {
     return NULL;
   }
   else {
-    return _Upgrader_Base::instance();
+    return CRM_Offlinerecurring_Upgrader_Base::instance();
   }
 }
 
