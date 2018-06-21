29ad78e22ec0a600e7ae472e3dd87054ac2fd11f
Pradeep Nayak <Pradeep Nayak>
2018-06-21 11:39:38 +0100

    removed cruft code, used api to create/update recurring contribution

--- CRM/OfflineRecurring/Form/RecurringContribution.php
+++ CRM/OfflineRecurring/Form/RecurringContribution.php
@@ -95,33 +95,6 @@ class CRM_OfflineRecurring_Form_RecurringContribution extends CRM_Core_Form {
       );
       $this->addElement('checkbox', 'move_recurring_record', ts('Move Recurring Record?'));
       $this->addElement('checkbox', 'move_existing_contributions', ts('Move Existing Contributions?'));
-      // Check if membership is linked with the recur record and allowed to be moved to different membership
-      // NOTE: 'membership_id' is not in 'civicrm_contribution_recur' table by default
-      if(CRM_Core_DAO::checkFieldExists('civicrm_contribution_recur', 'membership_id')) {
-        // Get memberships of the contact
-        // This will allow the recur record to be attached to a different membership of the same contact
-        $memberships = civicrm_api3(
-          'membership',
-          'get',
-          ['contact_id' => $cid]
-        );
-        if (!empty($memberships['values'])) {
-          $existingMemberships = ['0' => '- select -'];
-          foreach ($memberships['values'] as $membership_id => $membership_details) {
-            // Replace with GetLabel
-            $membershipStatus = CRM_Core_PseudoConstant::getLabel(
-              'CRM_Membership_BAO_Membership',
-              'status_id',
-              $membership_details['status_id']
-            );
-            $existingMemberships[$membership_id] = $membership_details['membership_name']
-            .' / '. $membershipStatus
-            .' / '. $membership_details['start_date']
-            .' / '. $membership_details['end_date'];
-          }
-          $this->add('select', 'membership_record', ts('Membership'), $existingMemberships);
-        }
-      }
     }
     $this->addMoney('amount',
       ts('Amount'),
@@ -166,9 +139,9 @@ class CRM_OfflineRecurring_Form_RecurringContribution extends CRM_Core_Form {
   */
   public static function formRule($fields, $files, $self) {
     $errors = [];
-    if (!empty($values['start_date']) && !empty($values['end_date'])) {
-      $start = CRM_Utils_Date::processDate($values['start_date']);
-      $end = CRM_Utils_Date::processDate($values['end_date']);
+    if (!empty($fields['start_date']) && !empty($fields['end_date'])) {
+      $start = CRM_Utils_Date::processDate($fields['start_date']);
+      $end = CRM_Utils_Date::processDate($fields['end_date']);
       if (($end < $start) && ($end != 0)) {
         $errors['end_date'] = ts('End date should be after Start date');
       }
@@ -183,152 +156,48 @@ class CRM_OfflineRecurring_Form_RecurringContribution extends CRM_Core_Form {
   * @return None
   */
   public function postProcess() {
-
-    $config = CRM_Core_Config::singleton();
     $params = $this->controller->exportValues();
     $params['recur_id'] = $this->_submitValues['recur_id'];
-
-        if(!empty($params['start_date']))
-            $start_date = CRM_Utils_Date::processDate($params['start_date']);
-        if(!empty($params['end_date']))
-            $end_date = CRM_Utils_Date::processDate($params['end_date']);
-        if(!empty($params['next_sched_contribution']))
-            $next_sched_contribution = CRM_Utils_Date::processDate($params['next_sched_contribution']);
-
-        if ($params['action'] == 'add') {
-
-            $fields = "id, contact_id, amount, frequency_interval, frequency_unit, invoice_id, trxn_id, currency, create_date, start_date, next_sched_contribution";
-            if (_offlinerecurring_getCRMVersion() >= 4.4)
-                $fields .= '_date';
-
-            $values       = "NULL, %1, %2, %3, %4, %5, %6, %7, %8, %9, %10";
-            $invoice_id   = md5(uniqid(rand(), TRUE));
-
-            $recur_params = array(
-                1 =>  array($params['cid'],                'Integer'),
-                2 =>  array($params['amount'],             'String'),
-                3 =>  array($params['frequency_interval'], 'String'),
-                4 =>  array($params['frequency_unit'],     'String'),
-                5 =>  array($invoice_id,                   'String'),
-                6 =>  array($invoice_id,                   'String'),
-                7 =>  array($config->defaultCurrency,      'String'),
-                8 =>  array(date('YmdHis'),                'String'),
-                9 =>  array($start_date,                   'String'),
-                10 => array($next_sched_contribution,      'String')
-            );
-
-            if (isset($end_date)) {
-                $fields          .= ", end_date";
-                $values          .= ", %11";
-                $recur_params[11] = array($end_date, 'String');
-            }
-
-            $sql    = sprintf("INSERT INTO civicrm_contribution_recur (%s) VALUES (%s)", $fields, $values);
-            $status = ts('Recurring Contribution setup successfully');
-
-        } elseif ($params['action'] == 'update') {
-
-            $sql = "UPDATE civicrm_contribution_recur SET amount = %1, frequency_interval = %2, frequency_unit = %3, start_date = %4, next_sched_contribution = %5, modified_date = %6";
-            if (_offlinerecurring_getCRMVersion() >= 4.4)
-                $sql = str_replace('next_sched_contribution', 'next_sched_contribution_date', $sql);
-
-            $recur_params = array(
-                1 =>  array($params['amount'],             'String'),
-                2 =>  array($params['frequency_interval'], 'String'),
-                3 =>  array($params['frequency_unit'],     'String'),
-                4 =>  array($start_date,                   'String'),
-                5 =>  array($next_sched_contribution,      'String'),
-                6 =>  array(date('YmdHis'),                'String'),
-                7 =>  array($params['recur_id'],           'Integer')
-            );
-
-            if (isset($end_date)) {
-                $sql            .= ", end_date = %8";
-                $recur_params[8] = array($end_date, 'String');
-            }
-
-            $sql   .= ' WHERE id = %7';
-            $status = ts('Recurring Contribution updated');
-
-            // Moving recurring record to another contact, if 'Move Recurring Record?' is ticked
-            $move_recurring_record = $this->_submitValues['move_recurring_record'];
-            if ($move_recurring_record == 1) {
-              $move_existing_contributions = $this->_submitValues['move_existing_contributions'];
-              $selected_cid = $this->_submitValues['selected_cid'];
-
-              if (!empty($selected_cid)) {
-                // Update contact id in civicrm_contribution_recur table
-                $update_recur_sql = "UPDATE civicrm_contribution_recur SET contact_id = %1 WHERE id = %2";
-                $update_recur_params = array(
-                  1 =>  array($selected_cid,      'Integer'),
-                  2 =>  array($params['recur_id'],  'Integer')
-                );
-                CRM_Core_DAO::executeQuery($update_recur_sql, $update_recur_params);
-
-                // Update contact id in civicrm_contribution table, if 'Move Existing Contributions?' is ticked
-                if ($move_existing_contributions == 1) {
-                  $update_contribution_sql = "UPDATE civicrm_contribution SET contact_id = %1 WHERE contribution_recur_id = %2";
-                  CRM_Core_DAO::executeQuery($update_contribution_sql, $update_recur_params);
-                }
-
-                // Move recurring record to another membership
-                $membership_record = $this->_submitValues['membership_record'];
-                if (CRM_Core_DAO::checkFieldExists('civicrm_contribution_recur', 'membership_id')) {
-                  // Update membership id in civicrm_contribution_recur table
-                  $update_membership_sql = "UPDATE civicrm_contribution_recur SET membership_id = %1 WHERE id = %2";
-                  $update_membership_params = array(
-                    1 =>  array($membership_record,   'Integer'),
-                    2 =>  array($params['recur_id'],  'Integer')
-                  );
-                  CRM_Core_DAO::executeQuery($update_membership_sql, $update_membership_params);
-
-                  // Move membership payments if 'Move Existing Contributions?' is ticked
-                  if ($move_existing_contributions == 1 && $membership_record > 0 ) {
-
-                    // Create/Update membership payment
-                    // Check if the membership payment exists
-                    // if not create new one
-                    $contributions_sql = "SELECT cc.id , mp.contribution_id, mp.id as payment_id FROM civicrm_contribution cc LEFT JOIN civicrm_membership_payment mp ON mp.contribution_id = cc.id WHERE cc.contribution_recur_id = %1";
-                    $contributions_params = array(
-                      1 =>  array($params['recur_id'],  'Integer')
-                    );
-                    $contributions_dao = CRM_Core_DAO::executeQuery($contributions_sql, $contributions_params);
-                    while($contributions_dao->fetch()) {
-                      if (!empty($contributions_dao->contribution_id)) {
-                        //Update membership payment
-                        $update_membership_payment_sql = "UPDATE civicrm_membership_payment SET membership_id = %1 WHERE id = %2";
-                        $update_membership_payment_params = array(
-                          1 =>  array($membership_record,   'Integer'),
-                          2 =>  array($contributions_dao->payment_id,  'Integer')
-                        );
-                        CRM_Core_DAO::executeQuery($update_membership_payment_sql, $update_membership_payment_params);
-                      } else {
-                        //Insert membership payment
-                        $insert_membership_payment_sql = "INSERT INTO civicrm_membership_payment SET contribution_id = %2 , membership_id = %1";
-                        $insert_membership_payment_params = array(
-                          1 =>  array($membership_record,   'Integer'),
-                          2 =>  array($contributions_dao->id,  'Integer')
-                        );
-                        CRM_Core_DAO::executeQuery($insert_membership_payment_sql, $insert_membership_payment_params);
-                      }
-                    }
-                  }
-                }
-              }
-            }
-        }
-
-        CRM_Core_DAO::executeQuery($sql, $recur_params);
-        $recur_id = ($params['action'] == 'add' ? CRM_Core_DAO::singleValueQuery('SELECT LAST_INSERT_ID()') : $params['recur_id']);
-        CRM_Core_DAO::executeQuery("REPLACE INTO civicrm_contribution_recur_offline (recur_id) VALUES (%1)", array(1 => array($recur_id, 'Integer')));
-
-        $session = CRM_Core_Session::singleton();
-        //CRM_Core_Session::setStatus($status);
-        CRM_Core_Session::setStatus($status, ts('Complete'), 'success');
-        //CRM_Utils_System::redirect(
-        //    CRM_Utils_System::url('civicrm/contact/view', 'reset=1&cid=' . $params['cid'], FALSE, null, FALSE, TRUE)
-        //);
-
+    $hash = md5(uniqid(rand(), TRUE));
+    $recurParams = [
+      'contact_id' => $params['contact_id'],
+      'amount' => $params['amount'],
+      'currency' => $params['currency'],
+      'frequency_unit' => $params['frequency_unit'],
+      'frequency_interval' => $params['frequency_interval'],
+      'installments' => $params['installments'],
+      'start_date' => $params['start_date'],
+      'create_date' => date('Ymd'),
+      'end_date' => $params['end_date'],
+      'trxn_id' => $hash,
+      'invoice_id' => $hash,
+      'contribution_status_id' => 'In Progress',
+      'next_sched_contribution_date' => $params['next_sched_contribution'],
+      'financial_type_id' => $params['financial_type_id'],
+      'payment_instrument_id' => $params['payment_instrument_id'],
+    ];
+    if (!empty($this->_id)) {
+      $recurParams['id'] = $this->_id;
+    }
+    foreach (['start_date','end_date','next_sched_contribution',] as $date) {
+      if (!empty($recurParams[$date])) {
+        $recurParams[$date] = CRM_Utils_Date::processDate($recurParams[$date]);
       }
+    }
+    $recurring = civicrm_api3('ContributionRecur', 'create', $recurParams);
+
+    if ($this->_action & CRM_Core_Action::UPDATE) {
+      // Moving recurring record to another contact, if 'Move Recurring Record?' is ticked
+      if (!empty($params['move_recurring_record'])) {
+        if (!empty($params['move_existing_contributions'])) {
+          // Update contact id in civicrm_contribution table, if 'Move Existing Contributions?' is ticked
+          // TODO:use api
+          $update_contribution_sql = "UPDATE civicrm_contribution SET contact_id = %1 WHERE contribution_recur_id = %2";
+          CRM_Core_DAO::executeQuery($update_contribution_sql, $update_recur_params);
+        }
+      }
+    }
+    CRM_OfflineRecurring_Form_RecurringContribution($recurring['id']);
+  }
 
 }
--- templates/CRM/OfflineRecurring/Form/RecurringContribution.tpl
+++ templates/CRM/OfflineRecurring/Form/RecurringContribution.tpl
@@ -9,7 +9,7 @@
     </div>
 
     <table class="form-layout-compressed">
-      <tr><td class="label">{$form.amount.label}</td><td>{$form.amount.html}</td><tr>
+      <tr><td class="label">{$form.amount.label}</td><td>{$form.currency.html} &nbsp; &nbsp;{$form.amount.html}</td><tr>
       <tr><td>&nbsp;</td><td>{$form.is_recur.html} {ts}{$form.frequency_interval.label}{/ts} &nbsp;{$form.frequency_interval.html} &nbsp; {$form.frequency_unit.html}</td></tr>
       <tr><td class="label">{$form.start_date.label}</td><td>{include file="CRM/common/jcalendar.tpl" elementName=start_date}</td></tr>
       <tr><td class="label">{$form.next_sched_contribution.label}</td><td>{include file="CRM/common/jcalendar.tpl" elementName=next_sched_contribution}<br />
@@ -73,14 +73,6 @@
                   <td class="label">{$form.contact_id.label}</td>
                   <td>{$form.contact_id.html}</td>
                 </tr>
-                {if $form.membership_record}
-                  <tr>
-                    <td class="label">{$form.membership_record.label}</td>
-                    <td>{$form.membership_record.html}<br />
-                      <sub>( Membership Type / Membership Status / Start Date / End Date )</sub>
-                    </td>
-                  </tr>
-                {/if}
                 <tr>
                   <td class="label">{$form.move_existing_contributions.label}</td>
                   <td>{$form.move_existing_contributions.html}</td>
@@ -100,92 +92,22 @@
 {literal}
 <script type="text/javascript">
 CRM.$(function() {
-    var $form = cj("form.{/literal}{$form.formClass}{literal}");
-    cj("#contact_id", $form).change(displayMemberships);
+  $('#move-recurring-table').hide();
 
-    function displayMemberships( ) {
-        var data = cj("#contact_id", $form).select2('data');
-        cj('input[name=selected_cid]').val(data.id);
-        CRM.api('Membership', 'get', {'contact_id': data.id},
-        {success: function(data) {
-            cj('#membership_record').find('option').remove();
-            cj('#membership_record').append(cj('<option>', {
-                value: '0',
-                text : '- select -'
-            }));
-            cj.each(data.values, function(key, value) {
-
-                // Get membership status label
-                var membershipStatusId = value.status_id;
-                var membershipStatuslabel = '';
-                CRM.api('MembershipStatus', 'getsingle', {'id': membershipStatusId},
-                  {success: function(memstatus_data) {
-                        cj('#membership_record').append(cj('<option>', {
-                            value: value.id,
-                            text : value.membership_name + ' / ' + memstatus_data.label + ' / ' + value.start_date + ' / ' + value.end_date
-                        }));
-                   }
-                });
-            });
-            cj('#membership_record').parents('tr').show();
-          },
-        }
-      );
+  $('#move_recurring_record').change(function(){
+    if ($('#move_recurring_record').is(':checked')) {
+      $('#move-recurring-table').show();
     }
+    else {
+      $('#move-recurring-table').hide();
+    }
+  });
 
-    /*var contactUrl = {/literal}"{crmURL p='civicrm/ajax/rest' q='className=CRM_Contact_Page_AJAX&fnName=getContactList&json=1&context=navigation' h=0 }"{literal};
-    cj( '#contact_name' ).autocomplete( contactUrl, {
-        width: 200,
-        selectFirst: false,
-        minChars:1,
-        matchContains: true,
-        delay: 400
-    }).result(function(event, data, formatted) {
-        var cid = data[1];
-        var name = data[0].split('::');
-        cj('input[name=selected_cid]').val(cid);
-        cj('#contact_name').val(name[0]);
-        CRM.api('Membership', 'get', {'contact_id': cid},
-        {success: function(data) {
-            cj('#membership_record').find('option').remove();
-            cj('#membership_record').append(cj('<option>', {
-                value: '',
-                text : '- select -'
-            }));
-            cj.each(data.values, function(key, value) {
-                cj('#membership_record').append(cj('<option>', {
-                    value: value.id,
-                    text : value.membership_name + '/' + value.start_date + '/' + value.end_date
-                }));
-            });
-            cj('#membership_record').parents('tr').show();
-          },
-        }
-      );
-    });*/
+  // Hide 'Move Existing Contributions?' field is no existing contributions available
+  {/literal}{if $existingContributionsAvailable eq 0}{literal}
+    $('#move_existing_contributions').prop('checked', false);
+    $('#move_existing_contributions').parent().parent().hide();
+  {/literal}{/if}{literal}
 });
-
-cj(document).ready(function(){
-
-    //cj('#membership_record').parents('tr').hide();
-
-    cj('#move-recurring-table').hide();
-
-    cj('#move_recurring_record').change(function(){
-        if (cj('#move_recurring_record').is(':checked')) {
-            cj('#move-recurring-table').show();
-        } else {
-            cj('#move-recurring-table').hide();
-        }
-    });
-
-    // Hide 'Move Existing Contributions?' field is no existing contributions available
-    {/literal}{if $existingContributionsAvailable eq 0}{literal}
-        cj('#move_existing_contributions').prop('checked', false);
-        cj('#move_existing_contributions').parent().parent().hide();
-    {/literal}{/if}{literal}
-
-});
-
 </script>
 {/literal}
